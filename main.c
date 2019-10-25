084e8082143f4b17181c7963396e876c68b145eb
Bodea Razvan-Marius <razvan.bodea2700@gmail.com>
2019-10-25 15:25:57 +0300

    FoodShop update

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
 int main() {
+    char firstName[25];
+    char lastName[25];
+    char phoneNumber[15];
+    char address[30];
     printf("Welcome to our Food Shop!\n");
+    printf("Please input your data\n");
+    printf("---First name:\n");
+    gets(firstName);
+    printf("---Last name:\n");
+    gets(lastName);
+    printf("---Phone number\n");
+    gets(phoneNumber);
+    printf("---Address\n");
+    gets(address);
     return 0;
 }
