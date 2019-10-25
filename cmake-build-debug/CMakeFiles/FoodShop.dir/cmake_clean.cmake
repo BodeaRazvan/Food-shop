0e4b8a9581a73f10410ff1ec5e421ef89f7f2bfd
Bodea Razvan-Marius <razvan.bodea2700@gmail.com>
2019-10-25 15:20:02 +0300

    FoodShop

---
+++ cmake-build-debug/CMakeFiles/FoodShop.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FoodShop.dir/main.c.obj"
+  "FoodShop.exe"
+  "FoodShop.exe.manifest"
+  "FoodShop.pdb"
+  "libFoodShop.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FoodShop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/FoodShop.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for FoodShop.
+# This may be replaced when dependencies are built.
